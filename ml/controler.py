0608d1aac8e22b71aed580ee1d4c8b1a684488b0
Alan <tyujn24@mail.ru>
2024-02-28 18:52:27 +0300

    first

---
+++ ml/controler.py
@@ -0,0 +1,10 @@
+from typing import Callable, Any
+from .user_context import UserContext
+
+
+
+
+class Controler:
+    def __init__(self, user_context: UserContext):
+        self.user_context = user_context
+        self.callback: dict[Any, Callable] = {}
---
+++ ml/user_context.py
@@ -0,0 +1,20 @@
+
+
+
+
+class UserData:
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        pass
+
+
+
+
+class UserContext:
+    def __init__(self):
+        pass
+
+    def get_user_data_by_id(self,id) -> UserData:
+        pass
